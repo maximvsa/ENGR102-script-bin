97e9b8110f26c6e392281c82718e5b8b5222c958
Maximus Amick <maximvs@tamu.edu>
2025-09-18 20:47:05 -0500

    Delete engr_102 directory

--- engr_102/individual_header_format.py
+++
@@ -1,8 +0,0 @@
-# By submitting this assignment, I agree to the following:
-#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on this assignment."
-#
-# Name:         YOUR NAME
-# Section:      YOUR SECTION NUMBER
-# Assignment:   THE ASSIGNMENT NUMBER (e.g. Lab 1b-2)
-# Date:         DAY MONTH YEAR
--- engr_102/practice_submission.py
+++
@@ -1,2 +0,0 @@
-print("Howdy, World!")
-# I don't like writing comments because I think your code should be readable as-is
